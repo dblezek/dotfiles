578ec242a22c8de81155398c7f3859df03bc80d1
Daniel Blezek <blezek.daniel@mayo.edu>
2017-08-08 09:21:22 -0500

    Updated modes

---
+++ emacs/.emacs.d/elpa/rib-mode-20170722.356/rib-mode-autoloads.el
@@ -0,0 +1,23 @@
+;;; rib-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "rib-mode" "rib-mode.el" (22904 31594 0 0))
+;;; Generated autoloads from rib-mode.el
+
+(add-to-list 'auto-mode-alist '("\\.rib\\'" . rib-mode))
+
+(autoload 'rib-mode "rib-mode" "\
+Major mode for editing RenderMan(R) Interface Bytestream ASCII files
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; rib-mode-autoloads.el ends here
---
+++ emacs/.emacs.d/elpa/rib-mode-20170722.356/rib-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rib-mode" "20170722.356" "RenderMan® Interface Bytestream (RIB) Major Mode" '((emacs "24")) :commit "4172e902fd66f235184c0eb6db7fd4a73dbd0866" :url "https://github.com/blezek/rib-mode")
