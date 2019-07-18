ad9c561d5ce86d59d5ec833ec32cbe88199004f2
Daniel Blezek <blezek.daniel@mayo.edu>
2019-07-18 15:42:48 -0500

    Adds TFS

--- bitbar/url.10m.sh
+++ bitbar/url.10m.sh
@@ -17,6 +17,9 @@ echo "OneDrive | bash=$0 param1=https://mctools-my.sharepoint.com/personal/bleze
 echo "rail | bash=$0 param1=http://qia-jira.mayo.edu/rail/rail terminal=false"
 # echo "rail-discuss | bash=$0 param1=http://qia-jira.mayo.edu/rail/rail-discuss/issues terminal=false"
 echo "railtoolbox | bash=$0 param1=http://qia-jira.mayo.edu/rail/railtoolbox terminal=false"
+
+echo "TFS | bash=$0 terminal=false param1=https://tfs.mayo.edu/tfs/MayoClinic/Radiology%20Operational%20Systems/Radiology%20AHI/_backlogs/taskboard?_a=requirements"
+
 echo "---"
 echo "trac | bash=$0 param1=http://ril-trac.mayo.edu/git-RIL terminal=false"
 echo "trac tickets | bash=$0 param1=http://ril-trac.mayo.edu/git-RIL/report/10 terminal=false"
