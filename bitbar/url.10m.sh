3129bd9fdf9e2bb7b08c5bb7ba6490121993fc98
Daniel Blezek <blezek.daniel@mayo.edu>
2019-08-13 10:15:30 -0500

    Updated link

--- bitbar/url.10m.sh
+++ bitbar/url.10m.sh
@@ -14,9 +14,9 @@ echo "url"
 
 echo "---"
 echo "OneDrive | bash=$0 param1=https://mctools-my.sharepoint.com/personal/blezek_daniel_mayo_edu/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fblezek%5Fdaniel%5Fmayo%5Fedu%2FDocuments%2FRAIL%20Documents terminal=false"
-echo "rail | bash=$0 param1=http://qia-jira.mayo.edu/rail/rail terminal=false"
-# echo "rail-discuss | bash=$0 param1=http://qia-jira.mayo.edu/rail/rail-discuss/issues terminal=false"
-echo "railtoolbox | bash=$0 param1=http://qia-jira.mayo.edu/rail/railtoolbox terminal=false"
+echo "rail | bash=$0 param1=http://railgit.mayo.edu/rail/rail terminal=false"
+# echo "rail-discuss | bash=$0 param1=http://railgit.mayo.edu/rail/rail-discuss/issues terminal=false"
+echo "railtoolbox | bash=$0 param1=http://railgit.mayo.edu/rail/railtoolbox terminal=false"
 
 echo "TFS | bash=$0 terminal=false param1=https://tfs.mayo.edu/tfs/MayoClinic/Radiology%20Operational%20Systems/Radiology%20AHI/_backlogs/taskboard?_a=requirements"
 
